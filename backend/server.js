35e379a852a4a187896e76b0a25ddaa7bd215074
Ashish Prabhu K <ashishprabhu2143@gmail.com>
2024-01-07 17:49:52 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,10 @@ app.use('/api/users', userRoutes);
 
 if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   );
 } else {
   app.get('/', (req, res) => {
